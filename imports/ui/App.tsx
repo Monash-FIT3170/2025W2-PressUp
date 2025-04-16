c182d18db53d7640b37a002db3683e4735dfc767
djgh1009 <danielgh1009@gmail.com>
2025-04-16 12:43:01 +1000

    Setup TailwindCSS v4

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -4,7 +4,7 @@ import { Info } from './Info';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1 class="text-2xl text-sky-500">Welcome to Meteor!</h1>
     <Hello />
     <Info />
   </div>
