c6d82ed54a66b72a9cfc3227dcf9d4b1460c5329
djgh1009 <danielgh1009@gmail.com>
2025-04-17 18:48:52 +1000

    Add basic Stock Item collection

---
+++ imports/api/stock_item.ts
@@ -0,0 +1,13 @@
+import { Mongo } from "meteor/mongo";
+import { DBEntry } from "./database";
+
+// TODO: This probably needs to include a field that indicates
+// what constitutes "low" in stock (i.e. percentage/fixed quantity set by user)
+export interface StockItem extends DBEntry {
+  name: string;
+  quantity: number;
+  location: string; // TODO: This may need to be its own collection?
+  supplier: string; // TODO: This definitely needs to be its own collection.
+}
+
+export const StockItemsCollection = new Mongo.Collection<StockItem>("stock_items");
