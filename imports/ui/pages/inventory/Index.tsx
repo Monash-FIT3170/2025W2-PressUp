c707ed86ee7c8212d7f67c6ccbe92afa857544dc
djgh1009 <danielgh1009@gmail.com>
2025-04-18 02:40:00 +1000

    Stock table styling

--- imports/ui/pages/inventory/Index.tsx
+++ imports/ui/pages/inventory/Index.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export const InventoryIndex = () => {
   return (
-    <>
+    <div id="inventory" className="flex flex-1">
       <Outlet />
-    </>
+    </div>
   );
 };
