de0c5410ac26697c06f28a119345c0deda7ef356
djgh1009 <danielgh1009@gmail.com>
2025-04-18 17:56:32 +1000

    Stock table as component and more succint mocking

--- imports/ui/pages/inventory/Stock.tsx
+++ imports/ui/pages/inventory/Stock.tsx
@@ -1,229 +1,35 @@
 import React from "react";
-import { Pill } from "../../components/Pill";
 import { StockItem } from "/imports/api/stock_item";
-import {
-  InStock,
-  LowInStock,
-  OutOfStock,
-} from "../../components/StatusSymbols";
+import { StockTable } from "../../components/StockTable";
+
+// TODO: Delete this mock function when integrating with API
+const mockStockItems = (amount: number) => {
+  const rand = (max: number) => Math.floor(Math.random() * max);
+  let result: StockItem[] = [];
+  for (let i = 0; i < amount; ++i) {
+    result.push({
+      _id: i.toString(),
+      name: [
+        "Coffee Beans",
+        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        "Almond Milk",
+      ][rand(3)],
+      quantity: [0, 99999999, 100, 10][rand(4)],
+      location: `Room ${["1029381290129083190238120312938190282038120381029819028", "1", "2", "33"][rand(4)]}`,
+      supplier: `Supplier ${["102938129089127012801238120128091238901289012890128", "1", "2", "727"][rand(4)]}`,
+      createdAt: new Date(),
+    });
+  }
+  return result;
+};
 
 export const StockPage = () => {
   // TODO: Get from API here
-  const stockItems: StockItem[] = [
-    {
-      _id: "1",
-      name: "Coffee Beans",
-      quantity: 10,
-      location: "Room 1",
-      supplier: "Supplier 1",
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      name: "Small Small Small Small Takeaway Cups",
-      quantity: 999999999999,
-      location: "Room 21232813018912",
-      supplier: "Supplier 1198230928311028301",
-      createdAt: new Date(),
-    },
-    {
-      _id: "3",
-      name: "Almond Milk",
-      quantity: 0,
-      location: "Room 1",
-      supplier: "Supplier 2",
-      createdAt: new Date(),
-    },
-    {
-      _id: "1",
-      name: "Coffee Beans",
-      quantity: 10,
-      location: "Room 1",
-      supplier: "Supplier 1",
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      name: "Small Small Small Small Takeaway Cups",
-      quantity: 999999999999,
-      location: "Room 21232813018912",
-      supplier: "Supplier 1198230928311028301",
-      createdAt: new Date(),
-    },
-    {
-      _id: "3",
-      name: "Almond Milk",
-      quantity: 0,
-      location: "Room 1",
-      supplier: "Supplier 2",
-      createdAt: new Date(),
-    },
-    {
-      _id: "1",
-      name: "Coffee Beans",
-      quantity: 10,
-      location: "Room 1",
-      supplier: "Supplier 1",
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      name: "Small Small Small Small Takeaway Cups",
-      quantity: 999999999999,
-      location: "Room 21232813018912",
-      supplier: "Supplier 1198230928311028301",
-      createdAt: new Date(),
-    },
-    {
-      _id: "3",
-      name: "Almond Milk",
-      quantity: 0,
-      location: "Room 1",
-      supplier: "Supplier 2",
-      createdAt: new Date(),
-    },
-    {
-      _id: "1",
-      name: "Coffee Beans",
-      quantity: 10,
-      location: "Room 1",
-      supplier: "Supplier 1",
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      name: "Small Small Small Small Takeaway Cups",
-      quantity: 999999999999,
-      location: "Room 21232813018912",
-      supplier: "Supplier 1198230928311028301",
-      createdAt: new Date(),
-    },
-    {
-      _id: "3",
-      name: "Almond Milk",
-      quantity: 0,
-      location: "Room 1",
-      supplier: "Supplier 2",
-      createdAt: new Date(),
-    },
-    {
-      _id: "1",
-      name: "Coffee Beans",
-      quantity: 10,
-      location: "Room 1",
-      supplier: "Supplier 1",
-      createdAt: new Date(),
-    },
-    {
-      _id: "2",
-      name: "Small Small Small Small Takeaway Cups",
-      quantity: 999999999999,
-      location: "Room 21232813018912",
-      supplier: "Supplier 1198230928311028301",
-      createdAt: new Date(),
-    },
-    {
-      _id: "3",
-      name: "Almond Milk",
-      quantity: 0,
-      location: "Room 1",
-      supplier: "Supplier 2",
-      createdAt: new Date(),
-    },
-  ];
-
-  // TODO: Make this dynamic based on user choice
-  const lowInStockThreshold = 10;
-
-  if (stockItems.length == 0) return <h1>No inventory items</h1>;
+  const stockItems: StockItem[] = mockStockItems(100);
 
   return (
     <div id="stock" className="flex flex-1">
-      <div id="grid-container" className="overflow-auto flex-1">
-        <div className="grid grid-cols-5 g gap-y-2 text-nowrap text-center grid-cols-[minmax(0,2fr)_min-content_1fr_min-content_1fr] text-red-900">
-          <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-l-lg sticky top-0 z-1 text-left">
-            Item Name
-            <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-          </div>
-          <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
-            Quantity
-            <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-          </div>
-          <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
-            Stock Room
-            <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-          </div>
-          <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
-            Status
-            <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-          </div>
-          <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-r-lg sticky top-0 z-1">
-            Supplier
-          </div>
-          {stockItems.map((item, i) => {
-            const statusPill =
-              item.quantity == 0 ? (
-                <Pill
-                  bgColour="bg-red-700"
-                  borderColour="border-red-700"
-                  textColour="text-white"
-                >
-                  Out of Stock
-                </Pill>
-              ) : item.quantity <= lowInStockThreshold ? (
-                <Pill
-                  bgColour="bg-sky-300"
-                  borderColour="border-sky-300"
-                  textColour="text-white"
-                >
-                  Low in Stock
-                </Pill>
-              ) : (
-                <Pill
-                  bgColour="bg-green-700"
-                  borderColour="border-green-700"
-                  textColour="text-white"
-                >
-                  In Stock
-                </Pill>
-              );
-
-            const stockIcon =
-              item.quantity == 0 ? (
-                <OutOfStock />
-              ) : item.quantity <= lowInStockThreshold ? (
-                <LowInStock />
-              ) : (
-                <InStock />
-              );
-
-            return (
-              <React.Fragment key={i}>
-                <div className="text-left truncate relative py-1 px-2">
-                  {item.name}
-                  <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-                </div>
-                <div className="relative py-1 px-2 flex justify-end">
-                  <span className="truncate max-w-full px-1">
-                    {item.quantity}
-                  </span>
-                  <span className="flex-shrink-0 self-center">{stockIcon}</span>
-                  <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-                </div>
-                <div className="truncate relative py-1 px-2">
-                  {item.location}
-                  <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-                </div>
-                <div className="relative py-1 px-2">
-                  {statusPill}
-                  <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-                </div>
-                <div className="truncate py-1 px-2">{item.supplier}</div>
-              </React.Fragment>
-            );
-          })}
-        </div>
-      </div>
+      <StockTable stockItems={stockItems} />
     </div>
   );
 };
