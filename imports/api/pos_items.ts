1d05db1d2473156e7d5a246387f9078fa3d98ac4
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-04-25 15:55:40 +1000

    Success create list of items with setting the path
    Success create list of items with setting the path
    
    - page location
    http://localhost:3000/pos/display
    
    interface, png, css/ all components are integrating well and it shows up on correct location as well

--- imports/api/pos_items.ts
+++ imports/api/pos_items.ts
@@ -10,3 +10,25 @@ export interface PosItem extends DBEntry {
 }
 
 export const PosItemsCollection = new Mongo.Collection<PosItem>("pos_items");
+
+
+export const mainPosItems = (count: number): PosItem[] => {
+    const names = ["Latte", "Mocha", "Cappuccino", "Flat White", "Macchiato", "Iced Latte", "Croissant", "Muffin", "Cookie"];
+    const prices = [4.0, 4.5, 5.0, 3.0, 5.5, 6.0, 7.0, 5.0, 4.5];
+    const imageNames = ["latte", "mocha", "cappuccino", "flat_white", "macchiato", "iced_latte", "croissant", "muffin", "cookie"];
+  
+    const result: PosItem[] = [];
+  
+    for (let i = 0; i < count; i++) {
+      const index = i % names.length;
+      result.push({
+        _id: (i + 1).toString(),
+        name: names[index],
+        price: prices[index],
+        imageUrl: `/menu_items/${imageNames[index]}.png`,
+        createdAt: new Date(),
+      });
+    }
+  
+    return result;
+  };
