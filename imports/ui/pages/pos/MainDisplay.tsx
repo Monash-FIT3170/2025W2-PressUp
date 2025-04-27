ee4356b973effc876637e6b2821162a6e5a7722b
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-04-27 23:22:04 +1000

    Fixed grid stretch issue
    Fixed grid stretch issue
    
    Detect the bug where the layout gets stretched when entities are deleted.
    
    So fix css style as fixed size.

--- imports/ui/pages/pos/MainDisplay.tsx
+++ imports/ui/pages/pos/MainDisplay.tsx
@@ -2,10 +2,10 @@ import { mainPosItems, PosItem } from "/imports/api/pos_items";
 import { PosItemCard } from "../../components/PosItemCard";
 
 export const MainDisplay = () => {
-    const posItems: PosItem[] = mainPosItems(12); 
+    const posItems: PosItem[] = mainPosItems(9); 
 
     return (
-      <div id="pos-display" className="grid grid-cols-3 gap-4 p-4">
+      <div id="pos-display" className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4 items-start">
         {posItems.map((item) => (
           <PosItemCard key={item._id} item={item} />
         ))}
