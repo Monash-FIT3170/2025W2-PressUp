ca1953972f8cffda037e51aaee0fadd51ff126ee
“Christina” <christinaburkebroder1ck@gmail.com>
2025-04-28 16:24:48 +1000

    add aditional attributes and methods for menu item, make quantity an optional param, and reflect these changes in main.ts

--- imports/api/MenuItemsCollection.ts
+++ imports/api/MenuItemsCollection.ts
@@ -3,8 +3,11 @@ import { DBEntry } from "./database";
 
 export interface MenuItem extends DBEntry {
   name: string;
-  quantity: number;
   ingredients: string[];
+  available: boolean;
+  quantity?: number;
+  price?: number;
+  category?: string[];
 }
 
 export const MenuItemsCollection = new Mongo.Collection<MenuItem>("menuItems");
