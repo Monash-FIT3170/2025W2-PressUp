64b67a6fc759f52de5bc6135161e67a58df653ef
Jessica Mark <jmar0069@student.monash.edu>
2025-04-30 14:48:13 +1000

    Move the form to a separate component

---
+++ imports/ui/components/AddItemForm.tsx
@@ -0,0 +1,80 @@
+import { ChangeEvent, useState } from "react";
+import { Supplier } from "/imports/api/supplier";
+
+const mockSuppliers = (amount: number) => {
+  let result: Supplier[] = [];
+  for (let i = 1; i < amount; i++) {
+    result.push({ name: `Mock Supplier ${i}` });
+  }
+  return result;
+};
+
+const suppliers: Supplier[] = mockSuppliers(10);
+
+export const AddItemForm = () => {
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-center p-4 w-100 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+        <h3 className="text-xl font-semibold text-rose-400 dark:text-white">
+          New Stock Item
+        </h3>
+      </div>
+      <div className="p-4 md:p-5">
+        <form className="space-y-4" action="#">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+              Item Name
+            </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Coffee"
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+              Quantity
+            </label>
+            <input
+              type="number"
+              placeholder="0"
+              className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+              Location
+            </label>
+            <input
+              placeholder="Storage Room 1"
+              className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+              Supplier
+            </label>
+            <select
+              value={selectedValue}
+              onChange={handleChange}
+              className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            >
+              <option value="">--Select supplier--</option>
+              {suppliers.map((supplier) => (
+                <option value={supplier.name}>{supplier.name}</option>
+              ))}
+            </select>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
