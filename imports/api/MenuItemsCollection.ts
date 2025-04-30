5fe9943ba49131ac0d12714ae0d807a79177129c
Alvin Fung <alvinfung0007@gmail.com>
2025-04-30 14:55:12 +1000

    Adjusting menuItem attributes

--- imports/api/MenuItemsCollection.ts
+++ imports/api/MenuItemsCollection.ts
@@ -5,6 +5,10 @@ export interface MenuItem extends DBEntry {
   name: string;
   quantity: number;
   ingredients: string[];
+  available: boolean;
+  price: number;
+  category?: string[];
+  image: string;
 }
 
 export const MenuItemsCollection = new Mongo.Collection<MenuItem>("menuItems");
