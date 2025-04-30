10a807465e2036880c9fe1b93bfe3ddd398f8292
Alvin Fung <alvinfung0007@gmail.com>
2025-04-30 14:54:21 +1000

    Adding additional menuItems methods

--- imports/api/menuItemsMethods.ts
+++ imports/api/menuItemsMethods.ts
@@ -3,6 +3,27 @@ import { MenuItem, MenuItemsCollection } from "./MenuItemsCollection";
 
 Meteor.methods({
   'menuItems.insert'(item: MenuItem) {
-    MenuItemsCollection.insert(item);
+    MenuItemsCollection.insertAsync(item);
+  },
+
+  'menuItems.delete'(itemName: string) {
+    if (!itemName) {
+      throw new Meteor.Error('invalid-name', 'Item name is required');
+    }
+    MenuItemsCollection.removeAsync({ name: itemName });
+  },
+
+  'menuItems.update'(itemName: string, updatedFields: Partial<MenuItem>) {
+    if (!itemName || !updatedFields) {
+      throw new Meteor.Error('invalid-arguments', 'Item name and updated fields are required');
+    }
+    MenuItemsCollection.updateAsync({ name: itemName }, {
+      $set: updatedFields
+    });
+  },
+
+  'menuItems.getAll'() {
+    return MenuItemsCollection.find().fetch();
   }
+  
 });
