c681786ab4831bb211e4cb331831a352f03f9a25
Jessica Mark <jmar0069@student.monash.edu>
2025-04-30 12:59:17 +1000

    Update input fields to match new item

--- imports/ui/pages/inventory/Stock.tsx
+++ imports/ui/pages/inventory/Stock.tsx
@@ -66,73 +66,41 @@ export const StockPage = () => {
       <Modal open={open} onClose={() => setOpen(false)}>
         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
           <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-            Sign in to our platform
+            Add new inventory item
           </h3>
         </div>
         <div className="p-4 md:p-5">
           <form className="space-y-4" action="#">
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Your email
+                Item Name
               </label>
               <input
-                type="email"
-                name="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="name@company.com"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-500 focus:border-rose-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Coffee"
                 required
               />
             </div>
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Your password
+                Quantity
               </label>
               <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="0"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-500 focus:border-rose-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 required
               />
             </div>
-            <div className="flex justify-between">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  Remember me
-                </label>
-              </div>
-              <a
-                href="#"
-                className="text-sm text-blue-700 hover:underline dark:text-blue-500"
+            <div className="grid grid-cols-2">
+              <button
+                onClick={() => setOpen(false)}
+                className="mr-2 w-full border border-gray-200 text-black bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-rose-400 dark:hover:bg-rose-500 dark:focus:ring-rose-600"
               >
-                Lost Password?
-              </a>
-            </div>
-            <button
-              type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Login to your account
-            </button>
-            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?{" "}
-              <a
-                href="#"
-                className="text-blue-700 hover:underline dark:text-blue-500"
-              >
-                Create account
-              </a>
+                Cancel
+              </button>
+              <button className="ml-2 w-full text-white bg-rose-400 hover:bg-rose-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-rose-400 dark:hover:bg-rose-500 dark:focus:ring-rose-600">
+                Add item
+              </button>
             </div>
           </form>
         </div>
