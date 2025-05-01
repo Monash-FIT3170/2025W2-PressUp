5fe434a0e75ed10b75a32371e00eb3b441ba77e0
djgh1009 <danielgh1009@gmail.com>
2025-05-02 02:23:30 +1000

    Implement Suppliers collection

---
+++ imports/api/suppliers/SuppliersCollection.ts
@@ -0,0 +1,13 @@
+import { Mongo } from "meteor/mongo";
+import { DBEntry } from "../database";
+
+export interface Supplier extends DBEntry {
+  name: string;
+  description: string;
+  pastOrderQty: number;
+  phone: string;
+  email: string;
+  goods: string[]; // TODO: This may need to be determined programmatically
+}
+
+export const SuppliersCollection = new Mongo.Collection<Supplier>("suppliers");
