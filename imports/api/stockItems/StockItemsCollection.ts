5d5b88914bef93e5503335dd3a82c4c38bab00e5
djgh1009 <danielgh1009@gmail.com>
2025-05-02 12:12:47 +1000

    Reference Supplier in StockItem collection

--- imports/api/stockItems/StockItemsCollection.ts
+++ imports/api/stockItems/StockItemsCollection.ts
@@ -1,5 +1,6 @@
 import { Mongo } from "meteor/mongo";
 import { DBEntry } from "../database";
+import { Supplier } from "../suppliers/SuppliersCollection";
 
 // TODO: This probably needs to include a field that indicates
 // what constitutes "low" in stock (i.e. percentage/fixed quantity set by user)
@@ -7,7 +8,7 @@ export interface StockItem extends DBEntry {
   name: string;
   quantity: number;
   location: string; // TODO: This may need to be its own collection?
-  supplier: string; // TODO: This definitely needs to be its own collection.
+  supplier: Supplier | null;
 }
 
 export const StockItemsCollection = new Mongo.Collection<StockItem>("stockItems");
