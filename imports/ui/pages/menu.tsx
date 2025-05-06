0dc4f763c09c1aa91bcea5be3d188eefbe94caf1
“Christina” <christinaburkebroder1ck@gmail.com>
2025-05-06 17:46:54 +1000

    get menu items for menu page, not yet displayed

--- imports/ui/pages/menu.tsx
+++ imports/ui/pages/menu.tsx
@@ -1,6 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { Meteor } from "meteor/meteor";
 import Sidebar from "../components/AddItemSidebar";
+import { MenuItem } from '/imports/api/MenuItemsCollection';
 
-export const MenuPage = () => (
+export const MenuPage = () => {
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+
+  useEffect(() => {
+    Meteor.call('menuItems.getAll', (error: Meteor.Error | null, result: MenuItem[]) => {
+      if (error) {
+        console.error('Error fetching menu items:', error);
+      } else {
+        setMenuItems(result);
+      }
+    });
+  }, []);
+
+
+  return (
   <>
     <header className="header"></header>
     <main className="main" style={{ display: "flex", justifyContent: "flex-end" }}>
@@ -10,3 +27,4 @@ export const MenuPage = () => (
     </main>
   </>
 );
+}
