515c02c76cef0291fabed8188a4e36bcff14c768
JayReeMarine <jree0010@student.monash.edu>
2025-05-07 13:38:57 +1000

    Merge @fung-alvin work into branch
    Merge @fung-alvin work into branch

--- imports/ui/pages/pos/Index.tsx
+++ imports/ui/pages/pos/Index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export const PosIndex = () => {
   return (
-    <div id="pos" className="flex flex-1">
+    <div id="pos" className="flex flex-1 overflow-auto">
       <Outlet />
     </div>
   );
