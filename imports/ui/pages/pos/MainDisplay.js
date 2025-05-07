47ee08a28e6721468aa970b9cf0925a473b7b89c
JayReeMarine <jree0010@student.monash.edu>
2025-05-07 13:38:57 +1000

    Merge @fung-alvin work into branch
    Merge @fung-alvin work into branch

--- imports/ui/pages/pos/MainDisplay.js
+++ imports/ui/pages/pos/MainDisplay.js
@@ -2,6 +2,7 @@ import { MenuItem, MenuItemsCollection } from "../../../api/MenuItemsCollection"
 import { PosItemCard } from "../../components/PosItemCard";
 import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
 import { PosSideMenu } from "../../components/PosSideMenu";
+import { Meteor } from 'meteor/meteor';
 
 export const MainDisplay = () => {
     const isLoadingPosItems = useSubscribe("menuItems")
@@ -9,15 +10,14 @@ export const MainDisplay = () => {
 
     console.log(posItems)
 
-    const handleItemClick = (item) => {
-      item.quantity += 1; // 
-      alert(`Selected Menu: ${item.name} ($${item.price.toFixed(2)}) - Quantity: ${item.quantity}`);
-  };
+    const handleItemClick = (itemId) => {
+      Meteor.call("menuItems.updateQuantity", itemId, 1);
+    };
 
   return (  
-    <div className="grid grid-cols-5 sm:grid-cols-3 md:grid-cols-5 gap-4 p-4 items-start">
+    <div className="grid grid-cols-5 sm:grid-cols-2 md:grid-cols-5 gap-4 p-4 items-start overflow-auto">
       <div id="pos-main-display" className="col-span-4">
-        <div id="pos-display" className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4">
+        <div id="pos-display" className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
             {posItems.map((item) => (
               <div className="min-w-[160px]">
                 <PosItemCard item={item} onClick={handleItemClick} />
