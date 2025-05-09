c8242acb48159829e6a63d81b6de8c1a4ccbb8ef
djgh1009 <danielgh1009@gmail.com>
2025-05-09 15:30:44 +1000

    Improve mock data setup in database with Faker.js

--- imports/api/stockItems/StockItemsCollection.ts
+++ imports/api/stockItems/StockItemsCollection.ts
@@ -1,6 +1,5 @@
 import { Mongo } from "meteor/mongo";
 import { DBEntry } from "../database";
-import { Supplier } from "../suppliers/SuppliersCollection";
 
 // TODO: This probably needs to include a field that indicates
 // what constitutes "low" in stock (i.e. percentage/fixed quantity set by user)
@@ -8,7 +7,9 @@ export interface StockItem extends DBEntry {
   name: string;
   quantity: number;
   location: string; // TODO: This may need to be its own collection?
-  supplier: Supplier | null;
+  supplier: Mongo.ObjectID | null;
 }
 
-export const StockItemsCollection = new Mongo.Collection<StockItem>("stockItems");
+export const StockItemsCollection = new Mongo.Collection<StockItem>(
+  "stockItems",
+);
--- server/main.ts
+++ server/main.ts
@@ -1,63 +1,7 @@
 import { Meteor } from "meteor/meteor";
-import {
-  StockItemsCollection,
-  MenuItemsCollection,
-  SuppliersCollection,
-} from "/imports/api";
-
-
-// Method for inserting new items to menuItems collection
-const insertMenuItem = (itemName: string, ingredients: string[], available: boolean, quantity: number, price: number, image: string) =>
-  MenuItemsCollection.insertAsync({
-    name: itemName,
-    quantity: quantity,
-    ingredients: ingredients,
-    price: price,
-    available: available,
-    image: image,
-});
+import { mockDataGenerator } from "/imports/api/mockData";
+import "/imports/api";
 
 Meteor.startup(async () => {
-  Meteor.publish("stock_items", function () {
-    return StockItemsCollection.find();
-  });
-
-
-  Meteor.publish("menuItems", function() {
-    return MenuItemsCollection.find();
-  });
-
-
-  // Include for first intialisation of menuItems collection
-
-  // Dropping menuItems collection when application first runs
-  MenuItemsCollection.dropCollectionAsync()
-
-  // Hardcoded menu items for now
-  const pressUpMenuItems: [string, number, string[], number][] = [
-    // ["Soy Latte", Math.floor(Math.random()*10), ["Soy Milk", "Espresso"], 5.10],
-    // ["Beef Burger", Math.floor(Math.random()*10), ["Beef Patty", "Lettuce", "Cheese"], 6.80],
-    ["Mocha", Math.floor(Math.random()*10), ["Espresso", "Chocolate"], 4.80 ],
-    ["Cappuccino", Math.floor(Math.random()*10), ["Espresso", "Foamed Milk"], 4.90],
-    ["Flat White", Math.floor(Math.random()*10), ["Espresso", "Steamed Milk"], 5.00],
-    ["Macchiato", Math.floor(Math.random()*10), ["Espresso", "Foam"], 4.70],
-    ["Cookie", Math.floor(Math.random()*10), ["Flour", "Chocolate Chips"], 2.70],
-    ["Croissant", Math.floor(Math.random()*10), ["Flour", "Butter"], 5.10],
-    ["Muffin", Math.floor(Math.random()*10), ["Flour", "Butter", "Choc Chip"], 2.20],
-  ];
-
-  const noItems = 12
-
-  // Adding menu items to the menuItems collection
-  for (let i = 0; i < noItems; i++) {
-    let itemName = pressUpMenuItems[i%pressUpMenuItems.length][0];
-    let quantity = pressUpMenuItems[i%pressUpMenuItems.length][1];
-    let ingredients = pressUpMenuItems[i%pressUpMenuItems.length][2];
-    let price = pressUpMenuItems[i%pressUpMenuItems.length][3];
-    const image = `/menu_items/${itemName.toLowerCase()}.png`
-    
-    insertMenuItem(itemName, ingredients, true, quantity, price, image);
-  }
-
-
+  mockDataGenerator({});
 });
