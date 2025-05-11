c203c2bbe74aa40b499f0b71ff9b8d1f7d80e860
riyav1602 <126407677+rvya1602@users.noreply.github.com>
2025-05-11 21:45:00 +1000

    UI changes for cost saved and discount applied.

--- imports/ui/components/PosSideMenu.tsx
+++ imports/ui/components/PosSideMenu.tsx
@@ -98,12 +98,12 @@ export const PosSideMenu = ({ items }: PosSideMenuProps) => {
         {discountPercent !== 0 && (
           <div>
             <div className="flex justify-between items-center mb-2 bg-yellow-400 text-black text-sm rounded-lg p-1">
-              <span className="text-sm font-bold">Cost Saved</span>
-              <span className="text-sm font-bold">- ${savedAmount.toFixed(2)}</span>
-            </div>
-            <div className="flex justify-between items-center mb-2 bg-yellow-400 text-black text-sm rounded-lg p-1">
               <span className="text-sm font-bold">Discount Applied</span>
               <span className="text-sm font-bold">{discountPercent}%</span>
+            </div>
+            <div className="flex justify-between items-center mb-2 bg-yellow-200 text-black text-sm rounded-lg p-1">
+              <span className="text-sm font-bold">Cost Saved</span>
+              <span className="text-sm font-bold">- ${savedAmount.toFixed(2)}</span>
             </div>
           </div>
         )}
