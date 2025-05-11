5827914c4a85f9e7a6fb7fc8ffa2b18741ba0562
djgh1009 <danielgh1009@gmail.com>
2025-05-12 00:25:54 +1000

    Main header implementation

--- imports/ui/pages/Root.tsx
+++ imports/ui/pages/Root.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+import { Header, NavigationMenu } from "../components/Navigation";
+import { PageTitleProvider } from "../hooks/PageTitleContext";
 
-// TODO: Global Nav
-export const RootPage = () => (
-  <>
-    <header className="header">Nav</header>
-    <main className="main">
-      <Outlet />
-    </main>
-  </>
-);
+export const RootPage = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  return (
+    <PageTitleProvider>
+      <header>
+        <Header
+          onHamburgerClick={() => setMenuOpen(!menuOpen)}
+        />
+      </header>
+      <div className="flex-1 flex flex-rows overflow-hidden">
+        <nav>
+          <NavigationMenu show={menuOpen} />
+        </nav>
+        <main className="flex flex-1">
+          <Outlet />
+        </main>
+      </div>
+    </PageTitleProvider>
+  );
+};
