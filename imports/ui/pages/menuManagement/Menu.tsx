c58d6af1d0fae62406e3e05f71522d73571246e5
Milind Bhatt <bhatt.milind@outlook.com>
2025-05-13 18:34:03 +1000

    add item branch

---
+++ imports/ui/pages/menuManagement/Menu.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router";
+import { usePageTitle } from "../../hooks/PageTitleContext";
+
+export const Menu = () => {
+  // Set title
+  const [_, setPageTitle] = usePageTitle();
+  useEffect(() => {
+    setPageTitle("POS System");
+  }, [setPageTitle]);
+
+  return (
+    <div id="pos" className="flex flex-1 overflow-auto">
+      <Outlet />
+    </div>
+  );
+};
