6fe38e501490fd9e80d521221a2965a90b69a08b
Jessica Mark <jesskymark@gmail.com>
2025-05-14 18:22:51 +1000

    Implement supplier table

--- imports/ui/components/SupplierTable.tsx
+++ imports/ui/components/SupplierTable.tsx
@@ -1,104 +1,88 @@
 import React from "react";
-import { StockItemWithSupplier } from "../pages/inventory/types";
+import { Supplier } from "/imports/api/suppliers/SuppliersCollection";
+import { InfoSymbol, Cross } from "./symbols/GeneralSymbols";
 import { Pill } from "./Pill";
-import { OutOfStock, InStock, LowInStock } from "./symbols/StatusSymbols";
 
-interface StockTableProps {
-  stockItems: StockItemWithSupplier[];
+interface SupplierTableProps {
+  suppliers: Supplier[];
 }
 
-export const SupplierTable = ({ stockItems }: StockTableProps) => {
-  // TODO: Make this dynamic based on user choice
-  const lowInStockThreshold = 10;
-
-  if (stockItems.length == 0)
+export const SupplierTable = ({ suppliers }: SupplierTableProps) => {
+  if (suppliers.length == 0)
     return (
       <h2 className="flex-1 text-center font-bold text-xl text-red-900">
-        No inventory items
+        No supplier
       </h2>
     );
 
   return (
     <div id="grid-container" className="overflow-auto flex-1">
-      <div className="grid gap-y-2 text-nowrap text-center grid-cols-[minmax(0,2fr)_min-content_1fr_min-content_1fr] text-red-900">
-        <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-l-lg sticky top-0 z-1 text-left">
+      <div className="grid gap-y-2 text-nowrap text-center grid-cols-15 text-red-900">
+        <div className="absolute col-span-4 bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-l-lg sticky top-0 z-1 text-left">
           Supplier Name
           <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
         </div>
-        <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
+        <div className="col-span-4 bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
           Contact
           <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
         </div>
-        <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
+        <div className="col-span-3 bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
           Supplier Goods
           <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
         </div>
-        <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
+        <div className="col-span-2 bg-rose-200 py-1 px-2 border-y-3 border-rose-200 sticky top-0 z-1">
           Past Orders
           <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
         </div>
-        <div className="bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-r-lg sticky top-0 z-1">
+        <div className="col-span-2 bg-rose-200 py-1 px-2 border-y-3 border-rose-200 rounded-r-lg sticky top-0 z-1">
           PO
         </div>
-        {stockItems.map((item, i) => {
-          const statusPill =
-            item.quantity == 0 ? (
-              <Pill
-                bgColour="bg-red-700"
-                borderColour="border-red-700"
-                textColour="text-white"
-              >
-                Out of Stock
-              </Pill>
-            ) : item.quantity <= lowInStockThreshold ? (
-              <Pill
-                bgColour="bg-sky-300"
-                borderColour="border-sky-300"
-                textColour="text-white"
-              >
-                Low in Stock
-              </Pill>
-            ) : (
-              <Pill
-                bgColour="bg-green-700"
-                borderColour="border-green-700"
-                textColour="text-white"
-              >
-                In Stock
-              </Pill>
-            );
-
-          const stockIcon =
-            item.quantity == 0 ? (
-              <OutOfStock />
-            ) : item.quantity <= lowInStockThreshold ? (
-              <LowInStock />
-            ) : (
-              <InStock />
-            );
-
+        {suppliers.map((supplier, i) => {
           return (
             <React.Fragment key={i}>
-              <div className="text-left truncate relative py-1 px-2">
-                {item.name}
-                <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
-              </div>
-              <div className="relative py-1 px-2 flex justify-end">
+              <div className="col-span-4 text-left truncate relative py-1 px-2 flex items-center">
                 <span className="truncate max-w-full px-1">
-                  {item.quantity}
+                  {supplier.name}
                 </span>
-                <span className="flex-shrink-0 self-center">{stockIcon}</span>
+                <span className="flex-shrink-0 ml-auto cursor-pointer">
+                  {<InfoSymbol fill="#E76573" viewBox="0 0 24 24" />}
+                </span>
                 <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
               </div>
-              <div className="truncate relative py-1 px-2">
-                {item.location}
+              <div className="col-span-4 truncate relative py-1 px-2">
+                <div className="grid-rows-2">
+                  <div>
+                    <a
+                      href={`mailto:${supplier.email}`}
+                      className="text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      {supplier.email}
+                    </a>
+                  </div>
+                  <div>{supplier.phone}</div>
+                </div>
                 <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
               </div>
-              <div className="relative py-1 px-2">
-                {statusPill}
+              <div className="col-span-3 flex flex-wrap relative py-1 px-2">
+                {supplier.goods.map((good) => (
+                  <span className="bg-rose-400 border-rose-300 text-white rounded-sm text-xs m-1 w-max h-max px-2 py-1 inline-flex items-center">
+                    {good}
+                    <span className="pl-2 ml-auto cursor-pointer">
+                      {<Cross height="8px" width="8px" viewBox="0 0 14 14" />}
+                    </span>
+                  </span>
+                ))}
                 <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
               </div>
-              <div className="truncate py-1 px-2">{item.supplier?.name}</div>
+              <div className="col-span-2 relative py-1 px-2 flex items-center justify-center">
+                {supplier.pastOrderQty}
+                <div className="absolute bg-amber-700/25 w-px h-3/4 end-0 bottom-1/8" />
+              </div>
+              <div className="col-span-2 truncate py-1 px-2 flex items-center justify-center">
+                <button className="bg-pink-900 rounded-4xl text-white px-4 p-2 cursor-pointer ">
+                  Create PO
+                </button>
+              </div>
             </React.Fragment>
           );
         })}
