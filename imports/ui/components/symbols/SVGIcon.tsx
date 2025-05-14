98b9f43c2853d5a37df4202bbde0a6532612079c
Jessica Mark <jesskymark@gmail.com>
2025-05-14 18:22:39 +1000

    Add viewbox as parameter in SVG props

--- imports/ui/components/symbols/SVGIcon.tsx
+++ imports/ui/components/symbols/SVGIcon.tsx
@@ -3,17 +3,24 @@ import { ReactNode } from "react";
 export interface SVGProps {
   width?: string;
   height?: string;
+  viewBox?: string;
   fill?: string;
   children?: ReactNode;
 }
 
-export const SVGIcon = ({ width, height, fill, children }: SVGProps) => {
+export const SVGIcon = ({
+  width,
+  height,
+  viewBox,
+  fill,
+  children,
+}: SVGProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height={height ?? "24px"}
       width={width ?? "24px"}
-      viewBox="0 -960 960 960"
+      viewBox={viewBox ?? "0 -960 960 960"}
       fill={fill ?? "#FFFFFF"}
     >
       {children}
