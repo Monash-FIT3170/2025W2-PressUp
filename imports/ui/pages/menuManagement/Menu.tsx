0285c4da33d88f52d4edae00650ca6b79a1dd46d
Milind Bhatt <bhatt.milind@outlook.com>
2025-05-14 11:32:03 +1000

    menu

--- imports/ui/pages/menuManagement/Menu.tsx
+++ imports/ui/pages/menuManagement/Menu.tsx
@@ -1,17 +1,24 @@
 import { useEffect } from "react";
 import { Outlet } from "react-router";
 import { usePageTitle } from "../../hooks/PageTitleContext";
+import Sidebar from "../../components/AddItemSidebar";
 
 export const Menu = () => {
   // Set title
   const [_, setPageTitle] = usePageTitle();
   useEffect(() => {
-    setPageTitle("POS System");
+    setPageTitle("Menu Management System");
   }, [setPageTitle]);
 
   return (
     <div id="pos" className="flex flex-1 overflow-auto">
-      <Outlet />
+      {/* Add the Sidebar */}
+      <Sidebar />
+      
+      {/* Main content area */}
+      <div className="flex-1 overflow-auto">
+        <Outlet />
+      </div>
     </div>
   );
 };
