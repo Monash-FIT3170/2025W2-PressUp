1c76699a1d011e76d5cd498f88ddb633a64155ad
Jessica Mark <jesskymark@gmail.com>
2025-05-14 18:24:48 +1000

    Get suppliers from database to display in list

--- imports/ui/pages/inventory/Suppliers.tsx
+++ imports/ui/pages/inventory/Suppliers.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import { useSubscribe, useTracker } from "meteor/react-meteor-data";
-import { StockItemWithSupplier } from "./types";
-import {
-  StockItemsCollection,
-  Supplier,
-  SuppliersCollection,
-} from "/imports/api";
+import { Supplier, SuppliersCollection } from "/imports/api";
 import { SupplierTable } from "../../components/SupplierTable";
 import { Modal } from "../../components/Modal";
 import { AddItemForm } from "../../components/AddItemForm";
@@ -14,25 +9,17 @@ export const SuppliersPage = () => {
   const [open, setOpen] = useState(false);
   const [formResetKey, setFormResetKey] = useState(0);
 
-  const isLoadingStockItems = useSubscribe("stockItems.all") === false;
   const isLoadingSuppliers = useSubscribe("suppliers") === false;
 
-  const stockItems: StockItemWithSupplier[] = useTracker(() => {
-    const stockItems = StockItemsCollection.find(
+  const suppliers: Supplier[] = useTracker(() => {
+    const suppliers = SuppliersCollection.find(
       {},
       { sort: { name: 1 } },
     ).fetch();
     const result = [];
-    console.log(stockItems);
-    for (let stockItem of stockItems) {
-      let supplier: Supplier | null = null;
-      if (stockItem.supplier != null) {
-        supplier = SuppliersCollection.find({
-          _id: stockItem.supplier,
-        }).fetch()[0];
-        console.log(supplier);
-      }
-      result.push({ ...stockItem, supplier });
+    console.log(suppliers);
+    for (let supplier of suppliers) {
+      result.push({ ...supplier });
     }
     return result;
   });
@@ -49,16 +36,16 @@ export const SuppliersPage = () => {
       <div className="grid grid-cols-2">
         <button
           onClick={() => setOpen(true)}
-          className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 rounded-xl px-3 bg-rose-400 text-white cursor-pointer w-30 right-2 hover:bg-rose-500"
+          className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 rounded-xl px-3 bg-rose-400 text-white cursor-pointer w-right-2 hover:bg-rose-500"
         >
           Add Supplier
         </button>
       </div>
       <div id="stock" className="flex flex-1 flex-col overflow-auto">
-        {isLoadingStockItems || isLoadingSuppliers ? (
+        {isLoadingSuppliers ? (
           <p className="text-gray-400 p-4">Loading inventory...</p>
         ) : (
-          <SupplierTable stockItems={stockItems} />
+          <SupplierTable suppliers={suppliers} />
         )}
       </div>
 
