805f12c7a36033d0bc60ef82d473b9324b847378
saraalana <shop0007@student.monash.edu>
2025-05-16 18:13:17 +1000

    [WIP] added custom selects for add modal

--- imports/ui/components/AddMenuItem.tsx
+++ imports/ui/components/AddMenuItem.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Meteor } from "meteor/meteor";
-import { MenuItem} from "/imports/api";
+import { MenuItem } from "/imports/api";
 import { Modal } from "./Modal";
 
 interface AddItemModalProps {
@@ -20,6 +20,9 @@ export const AddMenuItem: React.FC<AddItemModalProps> = ({
     const [itemIngredients, setItemIngredients] = useState<string[]>([""]);
     const [itemCategory, setItemCategory] = useState<string[]>([""]);
     const [itemDiscount, setItemDiscount] = useState<number>(0);
+    const [availableIngredients, setAvailableIngredients] = useState<string[]>([""]);
+    const [availableCategories, setAvailableCategories] = useState<string[]>([""]);
+
 
     const updateItemIngredients = (newIngredient: string) => {
         var currentIngredients = itemIngredients;
@@ -28,6 +31,12 @@ export const AddMenuItem: React.FC<AddItemModalProps> = ({
         setItemIngredients(currentIngredients);
     }
 
+    const updateItemCategory = (newCategory: string) => {
+        var currentCategory = itemCategory;
+        currentCategory.push(newCategory);
+
+        setItemCategory(currentCategory);
+    }
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
@@ -52,58 +61,109 @@ export const AddMenuItem: React.FC<AddItemModalProps> = ({
             }
         )
 
-if (!item) return;
+        if (!item) return;
 
     };
 
+    useEffect(() => {
+        var ingredients = ["Milk", "Flour", "Eggs", "Bread", "Butter", "Strawberries", "Avocado", "Bacon", "Olive Oil", "Paprika", "Jam"];
+        var categories = ["Breakfast","Lunch","Hot Drinks","Iced Drinks","Pastries"];
+        setAvailableIngredients(ingredients);
+        setAvailableCategories(categories);
+    }, []);
+
     return (
         <Modal open={isOpen} onClose={onClose}>
-        <div className="p-4 md:p-5 max-h-[80vh] overflow-y-auto w-full">
-            <h2 className="text-xl font-semibold text-rose-400 mb-4">Edit Item</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-                <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Item Name</label>
-                <input
-                type="text"
-                value={itemName}
-                onChange={(e) => setItemName(e.target.value)}
-                className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
-                required
-                />
-            </div>
+            <div className="p-4 md:p-5 max-h-[80vh] overflow-y-auto w-full">
+                <h2 className="text-xl font-semibold text-rose-400 mb-4">Edit Item</h2>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Item Name</label>
+                        <input
+                            type="text"
+                            value={itemName}
+                            onChange={(e) => setItemName(e.target.value)}
+                            className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
+                            required
+                        />
+                    </div>
 
-            <div>
-                <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Price</label>
-                <input
-                type="number"
-                min={0}
-                step="0.01"
-                value={itemPrice}
-                onChange={(e) => setItemPrice(parseFloat(e.target.value) || 0)}
-                className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:text-white"
-                required
-                />
-            </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Price</label>
+                        <input
+                            type="number"
+                            min={0}
+                            step="0.01"
+                            value={itemPrice}
+                            onChange={(e) => setItemPrice(parseFloat(e.target.value) || 0)}
+                            className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:text-white"
+                            required
+                        />
+                    </div>
 
-            <div className="flex justify-end space-x-2 pt-4">
-                <button
-                type="button"
-                onClick={onClose}
-                className="bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-lg"
-                >
-                Cancel
-                </button>
-                <button
-                type="submit"
-                className="bg-rose-400 hover:bg-rose-500 text-white px-4 py-2 rounded-lg"
-                >
-                Save
-                </button>
-            </div>
-            </form>
-        </div>
-    </Modal>
-  );
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Ingredients</label>
+                        <select
+                        onChange={(e) => updateItemIngredients(e.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
+                        required
+                    >
+                        <option value="">--Select Ingredient--</option>
+                        {availableIngredients.map((ingredient, i) => (
+                            <option value={ingredient} key={i}>
+                                {ingredient}
+                            </option>
+                        ))}
+                    </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Category</label>
+                        <select
+                        onChange={(e) => updateItemCategory(e.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
+                        required
+                    >
+                        <option value="">--Select Category--</option>
+                        {availableCategories.map((category, i) => (
+                            <option value={category} key={i}>
+                                {category}
+                            </option>
+                        ))}
+                    </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">Discount</label>
+                        <input
+                            type="number"
+                            min={0}
+                            step="0.01"
+                            value={itemDiscount}
+                            onChange={(e) => setItemDiscount(parseFloat(e.target.value) || 0)}
+                            className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:text-white"
+                        />
+                    </div>
+
+                    <div className="flex justify-end space-x-2 pt-4">
+                        <button
+                            type="button"
+                            onClick={onClose}
+                            className="bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-lg"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            className="bg-rose-400 hover:bg-rose-500 text-white px-4 py-2 rounded-lg"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div >
+        </Modal >
+    );
 };
 
 // return (
