8d238b252607fd32dcdaf8230fefe3817f3f87e7
djgh1009 <danielgh1009@gmail.com>
2025-05-17 18:37:47 +1000

    PurchaseOrderForm submit

--- imports/api/purchaseOrders/purchaseOrdersMethods.ts
+++ imports/api/purchaseOrders/purchaseOrdersMethods.ts
@@ -1,3 +1,11 @@
 import { Meteor } from "meteor/meteor";
+import {
+  PurchaseOrder,
+  PurchaseOrdersCollection,
+} from "./PurchaseOrdersCollection";
 
-Meteor.methods({});
+Meteor.methods({
+  async "purchaseOrders.insert"(order: Omit<PurchaseOrder, "date">) {
+    await PurchaseOrdersCollection.insertAsync({ ...order, date: new Date() });
+  },
+});
