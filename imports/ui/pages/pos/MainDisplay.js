7cabeab332956f89a084895df577177b60477ce5
jacksonwtong <jackson.w.tong8@gmail.com>
2025-05-21 07:03:16 +1000

    Order shows on side panel, and MenuItems can be added to it

--- imports/ui/pages/pos/MainDisplay.js
+++ imports/ui/pages/pos/MainDisplay.js
@@ -3,15 +3,45 @@ import { PosItemCard } from "../../components/PosItemCard";
 import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
 import { PosSideMenu } from "../../components/PosSideMenu";
 import { Meteor } from 'meteor/meteor';
+import { useState } from "react";
 
 export const MainDisplay = () => {
     const isLoadingPosItems = useSubscribe("menuItems")
     const posItems = useTracker( () => MenuItemsCollection.find().fetch());
 
-    console.log(posItems)
+    // Current order, hard coded table number at the moment
+    const [order, setOrder] = useState({
+      menuItems: [],
+      totalPrice: 0,
+      tableNo: 1,
+    });
 
-    const handleItemClick = (itemId) => {
-      Meteor.call("menuItems.updateQuantity", itemId, 1);
+    const handleItemClick = (item) => {
+
+      const existing = order.menuItems.find((i) => i._id === item._id);
+      let updatedItems;
+
+      // If item is already in order, increment quantity, otherwise add normally
+      if (existing) {
+        updatedItems = order.menuItems.map((i) =>
+          i._id === item._id ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        updatedItems = [...order.menuItems, { ...item, quantity: 1 }];
+      }
+
+      // Calculate total price of order
+      const newTotal = updatedItems.reduce(
+        (sum, i) => sum + i.quantity * i.price,
+        0
+      );
+
+      // Update order status
+      setOrder({
+        ...order,
+        menuItems: updatedItems,
+        totalPrice: parseFloat(newTotal.toFixed(2)),
+      });
     };
 
   return (  
@@ -19,7 +49,7 @@ export const MainDisplay = () => {
       <div id="pos-main-display" className="col-span-4">
         <div id="pos-display" className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
             {posItems.map((item) => (
-              <div className="min-w-[160px]">
+              <div className="min-w-[160px]" key={item._id.toString()}>
                 <PosItemCard item={item} onClick={handleItemClick} />
               </div>
             ))}
@@ -27,7 +57,7 @@ export const MainDisplay = () => {
           </div>
       </div>
       <div id="pos-side-panel" className="col-span-1 ">
-        <PosSideMenu items={posItems}></PosSideMenu>
+        <PosSideMenu items={order.menuItems} total={order.totalPrice}></PosSideMenu>
       </div>
     </div>
     
