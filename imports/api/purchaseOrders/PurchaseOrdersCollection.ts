e2e17326aba18164cb7da35e069b2a6446409193
djgh1009 <danielgh1009@gmail.com>
2025-05-21 13:59:30 +1000

    Add multiple StockItems to a single PurchaseOrder

--- imports/api/purchaseOrders/PurchaseOrdersCollection.ts
+++ imports/api/purchaseOrders/PurchaseOrdersCollection.ts
@@ -1,10 +1,14 @@
 import { Mongo } from "meteor/mongo";
 import { DBEntry } from "../database";
 
-export interface PurchaseOrder extends DBEntry {
+export interface StockItemLine {
   stockItem: Mongo.ObjectID;
   quantity: number;
   cost: number;
+}
+
+export interface PurchaseOrder extends DBEntry {
+  stockItems: StockItemLine[];
   date: Date;
 }
 
