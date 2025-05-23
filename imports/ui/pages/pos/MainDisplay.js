e4ebace0aaee8f91fb47bb13e88d7cf7c01873e6
“Christina” <christinaburkebroder1ck@gmail.com>
2025-05-23 11:30:58 +1000

    show only available items on pos page

--- imports/ui/pages/pos/MainDisplay.js
+++ imports/ui/pages/pos/MainDisplay.js
@@ -64,25 +64,26 @@ export const MainDisplay = () => {
         0
       );
 
-      updateOrder(updatedItems);      
+      updateOrder(updatedItems);
     };
 
-  return (  
+  return (
     <div className="grid grid-cols-5 sm:grid-cols-2 md:grid-cols-5 gap-4 p-4 items-start overflow-auto">
       <div id="pos-main-display" className="col-span-4">
         <div id="pos-display" className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-            {posItems.map((item) => (
-              <div className="min-w-[160px]" key={item._id.toString()}>
-                <PosItemCard item={item} onClick={handleItemClick} />
-              </div>
-            ))}
-          
-          </div>
+          {posItems
+          .filter((item) => item.available)
+          .map((item) => (
+            <div className="min-w-[160px]" key={item._id.toString()}>
+              <PosItemCard item={item} onClick={handleItemClick} />
+            </div>
+          ))}
+        </div>
       </div>
       <div id="pos-side-panel" className="col-span-1 ">
         <PosSideMenu tableNo={order.tableNo} items={order.menuItems} total={order.totalPrice} onIncrease={handleIncrease} onDecrease={handleDecrease}></PosSideMenu>
       </div>
     </div>
-    
+
   );
 };
