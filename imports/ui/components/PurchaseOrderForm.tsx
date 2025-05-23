48f0dac766d6069dd2b32ffaf9375d78d1c0b01f
djgh1009 <danielgh1009@gmail.com>
2025-05-24 03:18:24 +1000

    Add update PO functionality

--- imports/ui/components/PurchaseOrderForm.tsx
+++ imports/ui/components/PurchaseOrderForm.tsx
@@ -1,43 +1,86 @@
 import { Meteor } from "meteor/meteor";
-import { useTracker, useSubscribe } from "meteor/react-meteor-data";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { Mongo } from "meteor/mongo";
+import { useTracker } from "meteor/react-meteor-data";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
 import {
   PurchaseOrder,
+  PurchaseOrdersCollection,
   StockItem,
   StockItemLine,
   StockItemsCollection,
   Supplier,
+  SuppliersCollection,
 } from "/imports/api";
 
 interface PurchaseOrderFormProps {
-  onSuccess: (purchaseOrder: Omit<PurchaseOrder, "date">) => void;
+  purchaseOrderId: Mongo.ObjectID;
+}
+
+interface PurchaseOrderWithSupplier extends Omit<PurchaseOrder, "supplier"> {
   supplier: Supplier;
 }
 
 export const PurchaseOrderForm = ({
-  onSuccess,
-  supplier,
+  purchaseOrderId,
 }: PurchaseOrderFormProps) => {
-  useSubscribe("stockItems.all");
-  const availableStockItems: { [index: string]: StockItem } = useTracker(() => {
-    const queryResult = StockItemsCollection.find(
-      { supplier: supplier._id },
-      { sort: { name: 1 } },
-    ).fetch();
-    let result: { [index: string]: StockItem } = {};
-    for (let stockItem of queryResult) {
-      result[String(stockItem._id)] = stockItem;
-    }
-    return result;
-  }, [supplier]);
-
   const [stockItems, setStockItems] = useState<StockItemLine[]>([]);
   const [selectedStockItem, setSelectedStockItem] = useState<StockItem | null>(
     null,
   );
   const [quantityStr, setQuantityStr] = useState<string>("");
   const [costStr, setCostStr] = useState<string>("");
+
+  const purchaseOrder: PurchaseOrderWithSupplier | null = useTracker(() => {
+    const purchaseOrdersSubscription = Meteor.subscribe("purchaseOrders");
+    const suppliersSubscription = Meteor.subscribe("suppliers");
+    if (!purchaseOrdersSubscription.ready() || !suppliersSubscription.ready())
+      return null;
+
+    const purchaseOrderQueryResult = PurchaseOrdersCollection.find(
+      {
+        _id: purchaseOrderId,
+      },
+      { limit: 1 },
+    ).fetch()[0];
+
+    if (!purchaseOrderQueryResult) {
+      console.error("Error fetching Purchase Order");
+      return null;
+    }
+
+    const supplierQueryResult = SuppliersCollection.find(
+      {
+        _id: purchaseOrderQueryResult?.supplier,
+      },
+      { limit: 1 },
+    ).fetch()[0];
+
+    if (!supplierQueryResult) {
+      console.error("Error fetching Supplier");
+      return null;
+    }
+
+    return { ...purchaseOrderQueryResult, supplier: supplierQueryResult };
+  }, [purchaseOrderId]);
+
+  const availableStockItems: { [index: string]: StockItem } = useTracker(() => {
+    const stockItemsSubscription = Meteor.subscribe("stockItems.all");
+    if (!stockItemsSubscription.ready()) return {};
+
+    // Get viable StockItems
+    const stockItemsQueryResult = StockItemsCollection.find(
+      { supplier: purchaseOrder?.supplier._id },
+      { sort: { name: 1 } },
+    ).fetch();
+
+    // Convert to a mapping of IDs to StockItems
+    let result: { [index: string]: StockItem } = {};
+    for (let stockItem of stockItemsQueryResult) {
+      result[String(stockItem._id)] = stockItem;
+    }
+    return result;
+  }, [purchaseOrder]);
 
   const costRegex = /^([1-9][0-9]*|0)?(\.[0-9]{0,2})?$/;
   const quantityRegex = /^[0-9]*$/;
@@ -96,7 +139,7 @@ export const PurchaseOrderForm = ({
 
   useEffect(() => {
     clearForm();
-  }, [supplier]);
+  }, [purchaseOrderId]);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -106,19 +149,17 @@ export const PurchaseOrderForm = ({
       return;
     }
 
-    const purchaseOrder: Omit<PurchaseOrder, "date"> = {
-      stockItems,
-    };
-
     Meteor.call(
-      "purchaseOrders.insert",
-      purchaseOrder,
+      "purchaseOrders.update",
+      {
+        id: purchaseOrderId,
+        stockItems,
+      },
       (error: Meteor.Error | undefined) => {
         if (error) {
-          alert("Error: " + error.reason);
+          console.error("Error: " + error.reason);
         } else {
-          clearForm();
-          onSuccess(purchaseOrder);
+          alert("Saved successfully!");
         }
       },
     );
@@ -128,7 +169,7 @@ export const PurchaseOrderForm = ({
   const handlePrint = useReactToPrint({ contentRef: printRef });
 
   return (
-    <div className="flex flex-col space-y-5 p-2" ref={printRef}>
+    <div className="flex flex-col space-y-5 p-2 dark:text-white" ref={printRef}>
       <div className="grid grid-cols-2">
         <div>
           {/* TODO: Information about company */}
@@ -143,14 +184,19 @@ export const PurchaseOrderForm = ({
           New Purchase Order
         </div>
       </div>
-      <div className="p-2 grid grid-cols-2 bg-gray-300">
+      <div className="p-2 grid grid-cols-2 bg-gray-300 text-black">
         <div>
           <div className="font-bold underline pb-2 text-lg">Vendor</div>
-          <div className="font-bold">{supplier.name}</div>
-          {supplier.address ? <div>Address: {supplier.address}</div> : ""}
-          {supplier.phone ? (
+          <div className="font-bold">{purchaseOrder?.supplier?.name}</div>
+          {purchaseOrder ? (
+            <div>Address: {purchaseOrder.supplier.address}</div>
+          ) : (
+            ""
+          )}
+          {purchaseOrder ? (
             <div>
-              <span className="font-bold">Phone:</span> {supplier.phone}
+              <span className="font-bold">Phone:</span>{" "}
+              {purchaseOrder.supplier.phone}
             </div>
           ) : (
             ""
@@ -158,32 +204,35 @@ export const PurchaseOrderForm = ({
         </div>
         <div className="text-right">
           <div>
-            <span className="font-bold">Purchase Order #:</span> 94738285
+            <span className="font-bold">Purchase Order #: </span>
+            {purchaseOrder?.number}
           </div>
           <div>
-            <span className="font-bold">Date:</span> 06/05/2025
+            <span className="font-bold">Date: </span>
+            {purchaseOrder?.date.toLocaleDateString()}
           </div>
         </div>
       </div>
       <div className="grid grid-cols-12 overflow-auto max-h-50 print:overflow-visible print:h-auto">
         {/* Headers */}
-        <div className="col-span-5 border-b-2 border-b-black border-r-1 border-r-[#6F597B] sticky z-1 top-0 bg-stone-100">
+        <div className="col-span-5 border-b-2 border-b-black border-r-1 border-r-[#6F597B] sticky z-1 top-0 bg-stone-100 dark:bg-neutral-800">
           Item
         </div>
-        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100">
+        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100 dark:bg-neutral-800">
           Quantity
         </div>
-        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100">
+        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100 dark:bg-neutral-800">
           Unit Cost
         </div>
-        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100">
+        <div className="col-span-2 border-b-2 border-black px-2 border-r-1 border-r-[#6F597B] text-center sticky z-1 top-0 bg-stone-100 dark:bg-neutral-800">
           Total Cost
         </div>
-        <div className="border-b-2 border-black px-2 text-center sticky z-1 top-0 bg-stone-100"></div>
+        <div className="border-b-2 border-black px-2 text-center sticky z-1 top-0 bg-stone-100 dark:bg-neutral-800"></div>
         {/* Form Section */}
         <div className="col-span-5 border-b-1 border-b-[#F4E2E3] border-r-1 border-r-[#6F597B] flex items-center print:hidden">
           <select
             onChange={(e) =>
+              availableStockItems &&
               setSelectedStockItem(availableStockItems[e.target.value])
             }
             required
@@ -193,17 +242,19 @@ export const PurchaseOrderForm = ({
             }
             value={selectedStockItem ? String(selectedStockItem._id) : ""}
           >
-            <option value="">
+            <option className="text-black" value="">
               {availableStockItems &&
-                Object.keys(availableStockItems).length > 0
+              Object.keys(availableStockItems).length > 0
                 ? "Select Item"
                 : "No associated goods available..."}
             </option>
-            {Object.values(availableStockItems).map((item, i) => (
-              <option value={String(item._id)} key={i}>
-                {item.name}
-              </option>
-            ))}
+            {availableStockItems
+              ? Object.values(availableStockItems).map((item, i) => (
+                  <option className="text-black" value={String(item._id)} key={i}>
+                    {item.name}
+                  </option>
+                ))
+              : null}
           </select>
         </div>
         <div className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center print:hidden">
@@ -250,42 +301,29 @@ export const PurchaseOrderForm = ({
         </div>
         {/* Goods */}
         {stockItems.map((stockItemLine, i) => (
-          <>
-            <div
-              className="col-span-5 border-b-1 border-b-[#F4E2E3] border-r-1 border-r-[#6F597B] flex items-center"
-              key={`ssiname-${i}`}
-            >
-              {availableStockItems[String(stockItemLine.stockItem)].name}
+          <React.Fragment key={i}>
+            <div className="col-span-5 border-b-1 border-b-[#F4E2E3] border-r-1 border-r-[#6F597B] flex items-center">
+              {availableStockItems
+                ? availableStockItems[String(stockItemLine.stockItem)]?.name
+                : null}
             </div>
-            <div
-              className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center"
-              key={`ssiqty-${i}`}
-            >
+            <div className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center">
               {stockItemLine.quantity}
             </div>
-            <div
-              className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center"
-              key={`ssicost-${i}`}
-            >
+            <div className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center">
               {stockItemLine.cost}
             </div>
-            <div
-              className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center truncate"
-              key={`ssitotalcost-${i}`}
-            >
+            <div className="col-span-2 border-b-1 border-b-[#F4E2E3] px-2 border-r-1 border-r-[#6F597B] flex items-center justify-center truncate">
               {totalCost(stockItemLine.quantity, stockItemLine.cost)}
             </div>
-            <div
-              className="border-b-1 border-b-[#F4E2E3] px-2 flex items-center justify-center"
-              key={`ssidel-${i}`}
-            >
+            <div className="border-b-1 border-b-[#F4E2E3] px-2 flex items-center justify-center">
               <button
                 className="m-2 ease-in-out transition-all duration-300 shadow-lg/20 cursor-pointer ml-4 text-white bg-red-400 hover:bg-red-500 focus:drop-shadow-none focus:ring-2 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-300 dark:hover:bg-red-400 dark:focus:ring-red-400 print:hidden"
                 onClick={() =>
                   setStockItems((prev) =>
                     prev.filter(
                       (s) =>
-                        s.stockItem != stockItemLine.stockItem &&
+                        s.stockItem != stockItemLine.stockItem ||
                         s.cost != stockItemLine.cost,
                     ),
                   )
@@ -294,16 +332,22 @@ export const PurchaseOrderForm = ({
                 x
               </button>
             </div>
-          </>
+          </React.Fragment>
         ))}
       </div>
       {/* Totals */}
       <div className="grid grid-cols-12 max-h-50 text-right">
         <div className="col-span-7 text-left flex items-end">
-          <button className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 ml-auto rounded-xl px-3 bg-[#A43375] text-white cursor-pointer w-right-2 hover:bg-rose-500 print:hidden">
+          <button
+            onClick={handleSubmit}
+            className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 ml-auto rounded-xl px-3 bg-[#A43375] text-white cursor-pointer w-right-2 hover:bg-rose-500 print:hidden"
+          >
             Save PO
           </button>
-          <button onClick={handlePrint} className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 ml-auto rounded-xl px-3 bg-[#A43375] text-white cursor-pointer w-right-2 hover:bg-rose-500 print:hidden">
+          <button
+            onClick={() => handlePrint()}
+            className="text-nowrap justify-self-end shadow-lg/20 ease-in-out transition-all duration-300 p-1 m-4 ml-auto rounded-xl px-3 bg-[#A43375] text-white cursor-pointer w-right-2 hover:bg-rose-500 print:hidden"
+          >
             Print
           </button>
           <div className="flex-1"></div>
