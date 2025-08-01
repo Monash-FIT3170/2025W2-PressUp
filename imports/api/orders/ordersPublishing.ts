303839dcd6b6e64078071d7ad97af4fe0c22ba08
djgh1009 <danielgh1009@gmail.com>
2025-08-02 04:26:33 +1000

    Guard publishing endpoints from logged-out access

--- imports/api/orders/ordersPublishing.ts
+++ imports/api/orders/ordersPublishing.ts
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { OrdersCollection } from "./OrdersCollection";
+import { requireLoginPublish } from "../accounts/wrappers";
 
-Meteor.publish("orders", function () {
+Meteor.publish("orders", requireLoginPublish(function () {
   return OrdersCollection.find();
-});
+}));
--- imports/api/transactions/transactionsPublishing.ts
+++ imports/api/transactions/transactionsPublishing.ts
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { TransactionsCollection } from "./TransactionsCollection";
+import { requireLoginPublish } from "../accounts/wrappers";
 
-Meteor.publish("transactions", function () {
+Meteor.publish("transactions", requireLoginPublish(function () {
   return TransactionsCollection.find();
-});
+}));
