fd25920af5022cfcd76446931b9e1938a38c1530
Alvin Fung <alvinfung0007@gmail.com>
2025-08-04 20:46:47 +1000

    Added createdAt as an attribute of Transaction

--- imports/api/transactions/TransactionsCollection.ts
+++ imports/api/transactions/TransactionsCollection.ts
@@ -5,6 +5,7 @@ import { Order } from "../orders/OrdersCollection";
 export interface Transaction extends DBEntry {
   tableNo: number;
   order: Order;
+  createdAt: Date;
 }
 
 export const TransactionsCollection = new Mongo.Collection<Transaction>("transactions");
