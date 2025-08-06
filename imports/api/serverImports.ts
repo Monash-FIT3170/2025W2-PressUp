880fc73555c00ed6179b4832e29359da1503544f
Alvin Fung <alvinfung0007@gmail.com>
2025-08-06 19:32:22 +1000

    Removing references to TransactionsCollection

--- imports/api/serverImports.ts
+++ imports/api/serverImports.ts
@@ -2,8 +2,6 @@ import "./menuItems/menuItemsMethods";
 import "./menuItems/menuItemsPublishing";
 import "./stockItems/stockItemsMethods";
 import "./stockItems/stockItemsPublishing";
-import "./transactions/transactionsPublishing";
-import "./transactions/transactionsMethods";
 import "./suppliers/suppliersPublishing";
 import "./orders/ordersPublishing";
 import "./suppliers/SupplierMethods";
