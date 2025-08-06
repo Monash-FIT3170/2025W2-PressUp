52dd6f9e98c2452c1e06ed92e5c28903ad55d061
Alvin Fung <alvinfung0007@gmail.com>
2025-08-07 09:55:27 +1000

    Adding attributes to TablesCollection

--- imports/api/tables/TablesCollection.ts
+++ imports/api/tables/TablesCollection.ts
@@ -4,6 +4,9 @@ import { DBEntry } from "../database";
 export interface Tables extends DBEntry {
   tableNo: number;
   orderNo?: number;
+  capacity: number;
+  occupied: boolean;
+  noOccupants: number;
 }
 
 export const TablesCollection = new Mongo.Collection<Tables>("tables");
