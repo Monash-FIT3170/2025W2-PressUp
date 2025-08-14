7887c63cb77e3a87b48feb4266b4ddfca7282329
djgh1009 <danielgh1009@gmail.com>
2025-08-15 08:19:32 +1000

    Add gap and close confirmation to PublishShiftForm

--- imports/ui/pages/staff/Roster.tsx
+++ imports/ui/pages/staff/Roster.tsx
@@ -3,17 +3,33 @@ import { Button } from "../../components/interaction/Button";
 import { Modal } from "../../components/Modal";
 import { PublishShiftForm } from "../../components/PublishShiftForm";
 import { RosterTable } from "../../components/RosterTable";
+import { ConfirmModal } from "../../components/ConfirmModal";
 
 export const RosterPage = () => {
   const [shiftModalOpen, setShiftModalOpen] = useState(false);
+  const [showShiftModalCloseConfirmation, setShowShiftModalCloseConfirmation] =
+    useState(false);
+  const onCloseShiftModalConfirm = () => {
+    setShiftModalOpen(false);
+    setShowShiftModalCloseConfirmation(false);
+  };
 
   return (
     <div className="flex flex-1 flex-col">
       <Button onClick={() => setShiftModalOpen(true)}>Publish Shift</Button>
-      <Modal open={shiftModalOpen} onClose={() => setShiftModalOpen(false)}>
+      <Modal
+        open={shiftModalOpen}
+        onClose={() => setShowShiftModalCloseConfirmation(true)}
+      >
         <PublishShiftForm onSuccess={() => setShiftModalOpen(false)} />
       </Modal>
       <RosterTable />
+      <ConfirmModal
+        open={showShiftModalCloseConfirmation}
+        message="Are you sure you want to discard your changes?"
+        onConfirm={onCloseShiftModalConfirm}
+        onCancel={() => setShowShiftModalCloseConfirmation(false)}
+      />
     </div>
   );
 };
