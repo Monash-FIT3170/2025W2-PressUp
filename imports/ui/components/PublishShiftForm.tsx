c14d4e9c18c6726bc40ec246ddbc0b7e8f5f7b7a
djgh1009 <danielgh1009@gmail.com>
2025-08-15 08:19:32 +1000

    Add gap and close confirmation to PublishShiftForm

--- imports/ui/components/PublishShiftForm.tsx
+++ imports/ui/components/PublishShiftForm.tsx
@@ -44,41 +44,43 @@ export const PublishShiftForm = ({ onSuccess }: PublishShiftFormProps) => {
 
   return (
     <Form title="Publish Shift" onSubmit={onSubmit}>
-      <div>
-        <Label>User</Label>
-        <Select
-          placeholder="--Select user--"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-          required
-        >
-          {users.map((user) => (
-            <option value={user._id}>{user.username}</option>
-          ))}
-        </Select>
-      </div>
-      <div>
-        <Label>Start</Label>
-        <Input
-          value={startTime}
-          type="datetime-local"
-          onChange={(e) => setStartTime(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <Label>End</Label>
-        <Input
-          value={endTime}
-          type="datetime-local"
-          onChange={(e) => setEndTime(e.target.value)}
-          required
-        />
-      </div>
-      <div className="p-4 w-2/3 self-center">
-        <Button type="submit" width="full">
-          Publish
-        </Button>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Label>User</Label>
+          <Select
+            placeholder="--Select user--"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            required
+          >
+            {users.map((user) => (
+              <option value={user._id}>{user.username}</option>
+            ))}
+          </Select>
+        </div>
+        <div>
+          <Label>Start</Label>
+          <Input
+            value={startTime}
+            type="datetime-local"
+            onChange={(e) => setStartTime(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <Label>End</Label>
+          <Input
+            value={endTime}
+            type="datetime-local"
+            onChange={(e) => setEndTime(e.target.value)}
+            required
+          />
+        </div>
+        <div className="p-4 w-2/3 self-center">
+          <Button type="submit" width="full">
+            Publish
+          </Button>
+        </div>
       </div>
     </Form>
   );
