a609e4af1d15a2d60cf9dd377bab79d6cddd7050
djgh1009 <danielgh1009@gmail.com>
2025-08-15 11:25:32 +1000

    Add Roster page title

--- imports/ui/pages/staff/Roster.tsx
+++ imports/ui/pages/staff/Roster.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../../components/interaction/Button";
 import { Modal } from "../../components/Modal";
 import { PublishShiftForm } from "../../components/PublishShiftForm";
 import { RosterTable } from "../../components/RosterTable";
 import { ConfirmModal } from "../../components/ConfirmModal";
+import { usePageTitle } from "../../hooks/PageTitleContext";
 
 export const RosterPage = () => {
+  const [_, setPageTitle] = usePageTitle();
+  useEffect(() => {
+    setPageTitle("Staff Management - Roster")
+  }, [setPageTitle])
+
   const [shiftModalOpen, setShiftModalOpen] = useState(false);
   const [showShiftModalCloseConfirmation, setShowShiftModalCloseConfirmation] =
     useState(false);
