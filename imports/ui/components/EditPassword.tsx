7ef0dc2261f54aa2750d2cf2d91550250e26f503
Jessica Mark <jesskymark@gmail.com>
2025-08-22 15:32:21 +1000

    Implement showing password toggle

--- imports/ui/components/EditPassword.tsx
+++ imports/ui/components/EditPassword.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { PressUpRole } from "/imports/api/accounts/roles";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 type UserState = {
   firstName: string;
@@ -16,8 +18,10 @@ export const EditPassword: React.FC<EditPasswordProps> = ({
   password,
   setPassword,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
+    <div className="relative">
       <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
         Change User Password
       </label>
@@ -28,8 +32,15 @@ export const EditPassword: React.FC<EditPasswordProps> = ({
         }
         className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
         placeholder="User Password"
-        type="password"
+        type={showPassword ? "text" : "password"}
       />
+      <button
+        type="button"
+        onClick={() => setShowPassword((prev) => !prev)}
+        className="absolute right-6 top-12 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+      >
+        {showPassword ? <FaEyeSlash /> : <FaEye />}
+      </button>
     </div>
   );
 };
