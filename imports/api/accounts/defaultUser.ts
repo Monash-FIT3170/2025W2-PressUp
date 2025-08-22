06190576c5c69989948c79226c9fdd0082da22f7
Jessica Mark <jesskymark@gmail.com>
2025-08-22 15:07:29 +1000

    Fix default user creation to add role only once user is definitely created

--- imports/api/accounts/defaultUser.ts
+++ imports/api/accounts/defaultUser.ts
@@ -3,23 +3,32 @@ import { Roles } from "meteor/alanning:roles";
 import { Meteor } from "meteor/meteor";
 import { PressUpRole } from "./roles";
 
+
 export const createDefaultUser = async () => {
   const username =
     process.env.DEFAULT_USERNAME ||
     Meteor.settings?.private?.defaultUser?.username ||
     "admin";
+
   const password =
     process.env.DEFAULT_PASS ||
     Meteor.settings?.private?.defaultUser?.password ||
     "changeme";
 
-  if (!(await Meteor.users.findOneAsync({ username }))) {
-    const userId = await Accounts.createUserAsync({
-      username,
-      password,
-    });
-    await Roles.addUsersToRolesAsync(userId, PressUpRole.ADMIN);
+  // Try to find existing user
+  let user = await Meteor.users.findOneAsync({ username });
 
+  // If not found, create
+  if (!user) {
+    const userId = await Accounts.createUserAsync({ username, password });
     console.log(`Created default user '${username}'`);
+
+    user = await Meteor.users.findOneAsync(userId); // fetch full user doc
+  }
+
+  // Ensure role is set
+  if (!(await Roles.userIsInRoleAsync(user._id, PressUpRole.ADMIN))) {
+    await Roles.addUsersToRolesAsync(user._id, PressUpRole.ADMIN);
+    console.log(`Assigned '${username}' as ADMIN`);
   }
 };
