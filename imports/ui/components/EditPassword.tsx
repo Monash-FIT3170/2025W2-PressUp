5601032d1f258087e1af720e02b0da21066538be
Jessica Mark <jesskymark@gmail.com>
2025-08-22 14:03:40 +1000

    Modify EditPassword component to change state of password from parent

---
+++ imports/ui/components/EditPassword.tsx
@@ -0,0 +1,36 @@
+import { PressUpRole } from "/imports/api/accounts/roles";
+
+type UserState = {
+  firstName: string;
+  lastName: string;
+  role: PressUpRole;
+  password: string;
+};
+
+type EditPasswordProps = {
+  password: string;
+  setPassword: React.Dispatch<React.SetStateAction<UserState>>;
+};
+
+export const EditPassword: React.FC<EditPasswordProps> = ({
+  password,
+  setPassword,
+}) => {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+        User Password
+      </label>
+      <input
+        value={password}
+        onChange={(e) =>
+          setPassword((prev) => ({ ...prev, password: e.target.value }))
+        }
+        className="bg-gray-50 border border-gray-300 text-red-900 text-sm rounded-lg focus:ring-red-900 focus:border-red-900 block w-full p-2.5 dark:bg-stone-400 dark:border-stone-500 dark:placeholder-stone-300 dark:text-white"
+        placeholder="User Password"
+        type="password"
+        required
+      />
+    </div>
+  );
+};
