0b27615a11fd58559cd63919a94d1a0a09b4954b
Jessica Mark <jesskymark@gmail.com>
2025-08-28 15:42:23 +1000

    Add ForumPage component to communication page

---
+++ imports/ui/pages/staff/Communication.tsx
@@ -0,0 +1,9 @@
+import ForumPage from "../../components/ForumPage";
+
+export const CommunicationPage = () => {
+  return (
+    <div className="w-full">
+      <ForumPage></ForumPage>
+    </div>
+  );
+};
