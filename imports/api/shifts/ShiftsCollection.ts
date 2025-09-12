b4edb7bb4d95025f2c72d0d25e95308cbc393869
bingmo767 <chrisli91089767@gmail.com>
2025-09-12 13:10:22 +1000

    added Date

--- imports/api/shifts/ShiftsCollection.ts
+++ imports/api/shifts/ShiftsCollection.ts
@@ -9,15 +9,10 @@ export type ShiftTime = {
 
 export interface Shift extends DBEntry {
   user: string;
-<<<<<<< HEAD
+  date: Date;
   start: Date;
   end: Date;
   scheduledEnd?: Date; // This would be the planned end time
-=======
-  date: Date;
-  start: ShiftTime;
-  end: ShiftTime;
->>>>>>> 64cac7a5e19c32a4a58f5efe769f9f4c5198e0a0
 }
 
 export const ShiftsCollection = new Mongo.Collection<OmitDB<Shift>, Shift>(
