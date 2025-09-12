9b37586b1eabd1b4e384107491619aecca98ec9f
bingmo767 <chrisli91089767@gmail.com>
2025-09-12 12:45:55 +1000

    web config fix

--- imports/api/shifts/shiftsPublishing.ts
+++ imports/api/shifts/shiftsPublishing.ts
@@ -2,97 +2,76 @@ import { Meteor } from "meteor/meteor";
 import { Roles } from "meteor/alanning:roles";
 import { ShiftsCollection } from "./ShiftsCollection";
 import { requireLoginPublish } from "/imports/api/accounts/wrappers";
-import { RoleEnum as PressUpRole } from "/imports/api/accounts/roles";
+import { PressUpRole } from "/imports/api/accounts/roles";
 
-Meteor.publish(
-  "shifts.current",
-  requireLoginPublish(function () {
-    return ShiftsCollection.find({
-      user: this.userId,
-    });
-  }),
-);
+Meteor.publish("shifts.current", requireLoginPublish(function() {
+  return ShiftsCollection.find({
+    user: this.userId
+  });
+}));
 
 // for admins and manager: publish all users shifts
-Meteor.publish(
-  "shifts.all",
-  requireLoginPublish(async function () {
-    if (
-      !(await Roles.userIsInRoleAsync(this.userId, [
-        PressUpRole.ADMIN,
-        PressUpRole.MANAGER,
-      ]))
-    ) {
-      this.ready();
-      return;
-    }
+Meteor.publish("shifts.all", requireLoginPublish(async function() {
+  if (!await Roles.userIsInRoleAsync(this.userId, [PressUpRole.ADMIN, PressUpRole.MANAGER])) {
+    this.ready();
+    return;
+  }
 
-    return ShiftsCollection.find({});
-  }),
-);
+  return ShiftsCollection.find({});
+}));
 
-Meteor.publish(
-  "shifts.dateRange",
-  requireLoginPublish(async function (startDate: Date, endDate: Date) {
-    if (
-      !(await Roles.userIsInRoleAsync(this.userId, [
-        PressUpRole.ADMIN,
-        PressUpRole.MANAGER,
-      ]))
-    ) {
-      return ShiftsCollection.find({
-        user: this.userId,
-        $or: [
-          {
-            start: { $gte: startDate, $lte: endDate },
-          },
-          {
-            end: { $gte: startDate, $lte: endDate },
-          },
-          {
-            start: { $lte: startDate },
-            $or: [{ end: { $gte: endDate } }, { end: { $exists: false } }],
-          },
-        ],
-      });
-    }
-
+Meteor.publish("shifts.dateRange", requireLoginPublish(async function(startDate: Date, endDate: Date) {
+  if (!await Roles.userIsInRoleAsync(this.userId, [PressUpRole.ADMIN, PressUpRole.MANAGER])) {
     return ShiftsCollection.find({
+      user: this.userId,
       $or: [
         {
-          start: { $gte: startDate, $lte: endDate },
+          start: { $gte: startDate, $lte: endDate }
         },
         {
-          end: { $gte: startDate, $lte: endDate },
+          end: { $gte: startDate, $lte: endDate }
         },
         {
           start: { $lte: startDate },
-          $or: [{ end: { $gte: endDate } }, { end: { $exists: false } }],
-        },
-      ],
+          $or: [
+            { end: { $gte: endDate } },
+            { end: { $exists: false } } 
+          ]
+        }
+      ]
     });
-  }),
-);
+  }
+
+  return ShiftsCollection.find({
+    $or: [
+      {
+        start: { $gte: startDate, $lte: endDate }
+      },
+      {
+        end: { $gte: startDate, $lte: endDate }
+      },
+      {
+        start: { $lte: startDate },
+        $or: [
+          { end: { $gte: endDate } },
+          { end: { $exists: false } } 
+        ]
+      }
+    ]
+  });
+}));
 
 // publish active shifts
-Meteor.publish(
-  "shifts.active",
-  requireLoginPublish(async function () {
-    if (
-      !(await Roles.userIsInRoleAsync(this.userId, [
-        PressUpRole.ADMIN,
-        PressUpRole.MANAGER,
-      ]))
-    ) {
-      return ShiftsCollection.find({
-        user: this.userId,
-        end: { $exists: false },
-      });
-    }
-
-    // for managers and admins to see all active shifts
+Meteor.publish("shifts.active", requireLoginPublish(async function() {
+  if (!await Roles.userIsInRoleAsync(this.userId, [PressUpRole.ADMIN, PressUpRole.MANAGER])) {
     return ShiftsCollection.find({
-      end: { $exists: false },
+      user: this.userId,
+      end: { $exists: false }
     });
-  }),
-);
+  }
+
+  // for managers and admins to see all active shifts
+  return ShiftsCollection.find({
+    end: { $exists: false }
+  });
+}));
