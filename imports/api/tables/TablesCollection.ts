889c2e8dffda59900c02cfe4163398bbfaf63001
Jackson <jackson.w.t8@gmail.com>
2025-10-16 22:56:57 +1100

    Add bookings to TablesCollection

--- imports/api/tables/TablesCollection.ts
+++ imports/api/tables/TablesCollection.ts
@@ -1,6 +1,14 @@
 import { Mongo } from "meteor/mongo";
 import { DBEntry, IdType, OmitDB } from "../database";
 
+export interface TableBooking {
+  bookingDate: Date;
+  partySize: number;
+  customerName: string;
+  customerPhone: string;
+  notes?: string;
+}
+
 export interface Tables extends DBEntry {
   tableNo: number;
   activeOrderID: IdType | null;
@@ -8,6 +16,7 @@ export interface Tables extends DBEntry {
   capacity: number;
   isOccupied: boolean;
   noOccupants?: number;
+  bookings?: TableBooking[];
 }
 
 export const TablesCollection = new Mongo.Collection<OmitDB<Tables>, Tables>(
